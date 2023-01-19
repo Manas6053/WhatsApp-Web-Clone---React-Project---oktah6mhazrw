64f57a13074d62425aaccc582020cf54a7078eb3
Manas6053 <111265016+Manas6053@users.noreply.github.com>
2023-01-19 23:32:12 +0530

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -88,7 +88,7 @@ function Sidebar({ currentUser, signOut }) {
 
       <div className="sidebar-chat-list">
         {searchItem.length > 0
-          ? searchItem
+          ? searchItem|recentchat
           : friendList.map((friend) => (
               <Userprofile
                 name={friend.data().fullname}
